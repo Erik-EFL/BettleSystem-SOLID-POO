ec92932e8caa3ed7a430bd0ffd3997d108f39dae
erik lima <erik.lima.f@outlook.com>
2022-08-16 19:34:32 -0300

    fix(Battles): :bug: Corrigindo implementação da função para soluicionar erro no requisito

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -2,18 +2,18 @@ import Fighter from '../Fighter';
 import Battle from './Battle';
 
 export default class PVP extends Battle {
-  constructor(player: Fighter, private playerTwo: Fighter) {
+  constructor(player: Fighter, private _playerTwo: Fighter) {
     super(player);
   }
 
   isDead(): boolean {
-    return this.player.lifePoints === -1 || this.playerTwo.lifePoints === -1;
+    return (this.player.lifePoints === -1 || this._playerTwo.lifePoints === -1);
   }
 
-  override fight(): number {
+  fight(): number {
     while (!this.isDead()) {
-      this.player.attack(this.playerTwo);
-      this.playerTwo.attack(this.player);
+      this.player.attack(this._playerTwo);
+      this._playerTwo.attack(this.player);
     }
     return this.player.lifePoints === -1 ? -1 : 1;
   }
